c13b55aa200776ef0bd23d769286fc1f3ffa6ed7
fatemeh khoshbayan <f.khoshbayan@gmail.com>
2026-04-04 07:57:03 -0400

    feat: add projects data

---
+++ assets/projectsData.js
@@ -0,0 +1,47 @@
+export const projectsData = [
+  {
+    title: "Project 1",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae nisi necessitatibus provident perferendis ut laudantium harum suscipit doloremque, sunt officiis ab dicta excepturi ratione nesciunt facere omnis optio! Fuga, unde?",
+    imageAlt: "Project 1 Screenshot",
+    imageSrc: "https://placehold.jp/30/1a2227/f1e9da/600x400.png?text=Project+Screenshot+Coming+Soon",
+    link: "",
+    tags: "Web Design",
+  },
+  {
+    title: "Project 2",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae nisi necessitatibus provident perferendis ut laudantium harum suscipit doloremque, sunt officiis ab dicta excepturi ratione nesciunt facere omnis optio! Fuga, unde?",
+    imageAlt: "Project 2 Screenshot",
+    imageSrc: "https://placehold.jp/30/1a2227/f1e9da/600x400.png?text=Project+Screenshot+Coming+Soon",
+    link: "",
+    tags: "Health",
+  },
+  {
+    title: "Project 3",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae nisi necessitatibus provident perferendis ut laudantium harum suscipit doloremque, sunt officiis ab dicta excepturi ratione nesciunt facere omnis optio! Fuga, unde?",
+    imageAlt: "Project 3 Screenshot",
+    imageSrc: "https://placehold.jp/30/1a2227/f1e9da/600x400.png?text=Project+Screenshot+Coming+Soon",
+    link: "",
+    tags: "Extension",
+  },
+  {
+    title: "Project 4",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae nisi necessitatibus provident perferendis ut laudantium harum suscipit doloremque, sunt officiis ab dicta excepturi ratione nesciunt facere omnis optio! Fuga, unde?",
+    imageAlt: "Project 4 Screenshot",
+    imageSrc: "https://placehold.jp/30/1a2227/f1e9da/600x400.png?text=Project+Screenshot+Coming+Soon",
+    link: "",
+    tags: "Education",
+  },
+  {
+    title: "Project 5",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae nisi necessitatibus provident perferendis ut laudantium harum suscipit doloremque, sunt officiis ab dicta excepturi ratione nesciunt facere omnis optio! Fuga, unde?",
+    imageAlt: "Project 5 Screenshot", 
+    imageSrc: "https://placehold.jp/30/1a2227/f1e9da/600x400.png?text=Project+Screenshot+Coming+Soon",
+    link: "",
+    tags: "Web Design",
+  },
+];
